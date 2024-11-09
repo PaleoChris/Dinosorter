1795b00339d240968ab38a3d2480ac8eae89183a
Byte287 <107446147+Byte287@users.noreply.github.com>
2024-11-09 17:23:45 +0000

    starting work on config

---
+++ config.py
@@ -0,0 +1,68 @@
+#This is where the Theropod data will be called from to compara against
+    #user inputs in main.py. 
+
+#Herrerasaurus ischigualastensis -- PVSJ 373 (L)
+herrerasauruspaw = 0.9039451115
+
+#Eoraptor lunensis -- PVSJ 512 (R)
+eoraptorpaw = 1.272727273
+
+#Tawa hallae -- GR 242 (R)
+tawapaw = 0.7509157509
+
+#Syntarsus rhodesiensis -- QG 1
+syntarsuspaw = 0.8076923077
+
+#Coelophysis bauri -- TMP Red (L)
+coelophysispaw = 0.7232954545
+
+#Dilophosaurus wetherilli -- UCMP 37302 (L)
+dilophosauruspaw = 0.9808978032
+
+#Eodromaeus murphi -- PVSJ 562
+eodromaeuspaw = 1.125917343
+
+#Carnotaurus sastrei -- MACH-CH 894 (averaged)
+carnotauruspaw = 1.845345912
+
+#African noasaurid -- 
+africanpaw = 2.03960396
+
+#Xuanhanosaurus quilixiaensis -- IVPP V6729 (R)
+xuanhanosauruspaw = 1.107266436
+
+#Allosaurus fragilis -- USNM 4734
+allosauruspaw = 0.96
+
+#Torvosaurus tanneri -- BYU 725/2002
+torvosauruspaw = 1.659
+
+#Acrocanthosaurus atokensis -- NCSM 14345
+acrocanthosauruspaw = 1.659
+
+#Guanlong wucaii -- IVPP V14531 (L)
+guanlongpaw = 0.6328555678
+
+#Albertosaurus sarcophagus -- TMP 86.64.01
+albertosauruspaw = 0.2207792208
+
+#Gorgosaurus libratus -- TMP 91.36.500
+gorgosauruspaw = 0.8213716108
+
+#Daspletosaurus torosus -- TMP 2001.36.1
+daspletosauruspaw = 0.5921696574
+
+#Tarbosaurus bataar -- GIN 107/2 (L)
+tarbosauruspaw = 0.7202453988
+
+#Tyrannosaurus rex -- FMNH PR 2081
+tyrannosauruspaw = 0.6839134615
+
+#Yutyrannus huali -- ZCDM 5001
+yutyrannuspaw = 0.7180254301
+
+#Haplocheirus sollers -- IVPP V15988 (L)
+haplocheiruspaw = 0.5773195876
+
+#Gualicho shinyae -- 
+gualichopaw = 0.4160342717
--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import config
+herrerasauruscmp = 0.0
+eoraptorcmp = 0.0
+tawacmp = 0.0
+syntarsuscmp = 0.0
+coelophysiscmp = 0.0
+dilophosauruscmp = 0.0
+eodromaeuscmp = 0.0
+carnotauruscmp = 0.0
+africancmp = 0.0
+xuanhanosauruscmp = 0.0
+allosauruscmp = 0.0
+torvosauruscmp = 0.0
+acrocanthosauruscmp = 0.0
+guanlongcmp = 0.0
+albertosauruscmp = 0.0
+gorgosauruscmp = 0.0
+daspletosauruscmp = 0.0
+tarbosauruscmp = 0.0
+tyrannosauruscmp = 0.0
+yutyrannuscmp = 0.0
+haplocheiruscmp = 0.0
+gualichocmp = 0.0
+
+def pawcmp(input):
+    herrerasauruscmp += abs(config.herrerasauruspaw - input)
+    
+
 def main():
-    print("Hello, World!")
+    print("Hi! Welcome to the Dinosorter. Enter the requested measurement data to compare your specimen to our list of Theropods.")
+    PAW = float(input("Enter PAW: "))
+    BI = float(input("Enter BI: "))
+    HEI = float(input("Enter HEI: "))
+    OI = float(input("Enter OI: "))
+    RRI = float(input("Enter RRI: "))
+    MC3RI = float(input("Enter MC3RI: "))
+    HAA = float(input("Enter HAA: "))
+    print("calculating paw...")
+    pawcmp(PAW)
 
 if __name__ == "__main__":
     main()
