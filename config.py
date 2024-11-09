ef120fabbb45f2d7fdcabed31ca36d24a85ef608
Byte287 <107446147+Byte287@users.noreply.github.com>
2024-11-09 18:00:09 +0000

    all spreadsheet data entered

--- config.py
+++ config.py
@@ -1,68 +1,202 @@
 #This is where the Theropod data will be called from to compara against
     #user inputs in main.py. 
 
+#all measurements in the same unit (chris isn't sure)
+
 #Herrerasaurus ischigualastensis -- PVSJ 373 (L)
 herrerasauruspaw = 0.9039451115
+herrerasaurusbi = 0.7541871921
+herrerasaurushei = 0.2431855501
+herrerasaurusoi = 0.1021080369
+herrerasaurusrri = 0.07119529719
+herrerasaurusmc3ri = 0.1475128645
+herrerasaurushaa = 0.1362276728
 
 #Eoraptor lunensis -- PVSJ 512 (R)
 eoraptorpaw = 1.272727273
+eoraptorbi = 0.7485242031
+eoraptorhei = 0.2774498229
+eoraptoroi = 0.05792163543
+eoraptorrri = 0.09148264984
+eoraptormc3ri = 0.2373737374
+eoraptorhaa = 0.1499167956
 
 #Tawa hallae -- GR 242 (R)
 tawapaw = 0.7509157509
+tawabi = 0.8238188976
+tawahei = 0.2017716535
+tawaoi = 0.05995203837
+tawarri = 0.05615292712
+tawamc3ri = 0.1172161172
+tawahaa = 0.103957977
 
 #Syntarsus rhodesiensis -- QG 1
 syntarsuspaw = 0.8076923077
+syntarsusbi = 0.61
+syntarsushei = 0.22
+syntarsusoi = 0.2148942884
+syntarsusrri = 0.06557377049
+syntarsusmc3ri = 0.08076923077
+syntarsushaa = 0.1061084351
 
 #Coelophysis bauri -- TMP Red (L)
 coelophysispaw = 0.7232954545
+coelophysisbi = 0.6835637481
+coelophysishei = 0.1502304147
+coelophysisoi = 0.153937101
+coelophysisrri = 0.09438202247
+coelophysismc3ri = 0.1534090909
+coelophysishaa = 0.1146900829
 
 #Dilophosaurus wetherilli -- UCMP 37302 (L)
 dilophosauruspaw = 0.9808978032
+dilophosaurusbi = 0.7083941606
+dilophosaurushei = 0.2678832117
+dilophosaurusoi = 0.1645400345
+dilophosaurusrri = 0.1035548686
+dilophosaurusmc3ri = 0.1671442216
+dilophosaurushaa = 0.1539437517
 
 #Eodromaeus murphi -- PVSJ 562
 eodromaeuspaw = 1.125917343
+eodromaeusbi = 0.7769953052
+eodromaeushei = 0.2012676056
+eodromaeusoi = 0.1471497123
+eodromaeusrri = 0.08912386707
+eodromaeusmc3ri = 0.1698841699
+eodromaeushaa = 0.1176628059
 
 #Carnotaurus sastrei -- MACH-CH 894 (averaged)
 carnotauruspaw = 1.845345912
+carnotaurusbi = 0.2668421053
+carnotaurushei = 0.3148070175
+carnotaurusoi = 0.1522662379
+carnotaurusrri = 0.5431970037
+carnotaurusmc3ri = 0.4576058129
+carnotaurushaa = 0.2281469478
 
 #African noasaurid -- 
 africanpaw = 2.03960396
+africanbi = 0.3973856209
+africanhei = 0.2104575163
+africanoi = 0.1794871795
+africanrri = 0.1973684211
+africanmc3ri = 0.4356435644
+africanhaa = 0.1229028849
 
 #Xuanhanosaurus quilixiaensis -- IVPP V6729 (R)
 xuanhanosauruspaw = 1.107266436
+xuanhanosaurusbi = 0.7368421053
+xuanhanosaurushei = 0.2985964912
+xuanhanosaurusoi = 0.1025056948
+xuanhanosaurusrri = 0.1219047619
+xuanhanosaurusmc3ri = 0.2093425606
+xuanhanosaurushaa = 0.2128351215
 
 #Allosaurus fragilis -- USNM 4734
 allosauruspaw = 0.96
+allosaurusbi = 0.7161290323
+allosaurushei = 0.3225806452
+allosaurusoi = 0.2108432626
+allosaurusrri = 0.09009009009
+allosaurusmc3ri = 0.176
+allosaurushaa = 0.2362749842
 
 #Torvosaurus tanneri -- BYU 725/2002
 torvosauruspaw = 1.659
+torvosaurusbi = 0.4404651163
+torvosaurushei = 0.2988372093
+torvosaurusoi = 0.2549240293
+torvosaurusrri = 0.196937698
+torvosaurusmc3ri = 0.4233703001
+torvosaurushaa = 0.1722419218
 
 #Acrocanthosaurus atokensis -- NCSM 14345
 acrocanthosauruspaw = 1.659
+acrocanthosaurusbi = 0.5789473684
+acrocanthosaurushei = 0.3852631579
+acrocanthosaurusoi = 0.2429565794
+acrocanthosaurusrri = 0.1590909091
+acrocanthosaurusmc3ri = 0.275862069
+acrocanthosaurushaa = 0.2177222811
 
 #Guanlong wucaii -- IVPP V14531 (L)
 guanlongpaw = 0.6328555678
+guanlongbi = 0.723364486
+guanlonghei = 0.2504672897
+guanlongoi = 0.07430340557
+guanlongrri = 0.09689922481
+guanlongmc3ri = 0.1565600882
+guanlonghaa = 0.1194871783
 
 #Albertosaurus sarcophagus -- TMP 86.64.01
 albertosauruspaw = 0.2207792208
+albertosaurusbi = 0.465258216
+albertosaurushei = 0.1690140845
+albertosaurusoi = 0.2161594963
+albertosaurusrri = 0.1089808274
+albertosaurusmc3ri = 0.274258658
+albertosaurushaa = 0.1142781898
 
 #Gorgosaurus libratus -- TMP 91.36.500
 gorgosauruspaw = 0.8213716108
+gorgosaurusbi = 0.5639187575
+gorgosaurushei = 0.2212465153
+gorgosaurusoi = 0.3370786517
+gorgosaurusrri = 0.1387711864
+gorgosaurusmc3ri = 0.2488038278
+gorgosaurushaa = 0.1747395826
 
 #Daspletosaurus torosus -- TMP 2001.36.1
 daspletosauruspaw = 0.5921696574
+daspletosaurusbi = 0.5571830986
+daspletosaurushei = 0.2414084507
+daspletosaurusoi = 0.2699350266
+daspletosaurusrri = 0.2416582406
+daspletosaurusmc3ri = 0.4143556281
+daspletosaurushaa = 0.165792256
 
 #Tarbosaurus bataar -- GIN 107/2 (L)
 tarbosauruspaw = 0.7202453988
+tarbosaurusbi = 0.438028169
+tarbosaurushei = 0.1908450704
+tarbosaurusoi = 0.2993572084
+tarbosaurusrri = 0.1768488746
+tarbosaurusmc3ri = 0.290797546
+tarbosaurushaa = 0.129410282
 
 #Tyrannosaurus rex -- FMNH PR 2081
 tyrannosauruspaw = 0.6839134615
+tyrannosaurusbi = 0.4493506494
+tyrannosaurushei = 0.2846969697
+tyrannosaurusoi = 0.220090829
+tyrannosaurusrri = 0.1560693642
+tyrannosaurusmc3ri = 0.3076923077
+tyrannosaurushaa = 0.1685700921
 
 #Yutyrannus huali -- ZCDM 5001
 yutyrannuspaw = 0.7180254301
+yutyrannusbi = 0.6863354037
+yutyrannushei = 0.2534161491
+yutyrannusoi = 0.1217391304
+yutyrannusrri = 0.1194570136
+yutyrannusmc3ri = 0.172026926
+yutyrannushaa = 0.1067869349
 
 #Haplocheirus sollers -- IVPP V15988 (L)
 haplocheiruspaw = 0.5773195876
+haplocheirusbi = 0.7263513514
+haplocheirushei = 0.2867398649
+haplocheirusoi = 0.1333333333
+haplocheirusrri = 0.08255813953
+haplocheirusmc3ri = 0.14604811
+haplocheirushaa = 0.1501404277
 
 #Gualicho shinyae -- 
 gualichopaw = 0.4160342717
+gualichobi = 0.5387237762
+gualichohei = 0.1529370629
+gualichooi = 0.07451318055
+gualichorri = 0.08387906323
+gualichomc3ri = 0.1369277846
+gualichohaa = 0.07557332229
