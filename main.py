d74a998bd07f16e813e93ec9b04eb1d1ac617295
Byte287 <107446147+Byte287@users.noreply.github.com>
2024-11-09 18:52:13 +0000

    comparison functions created

--- main.py
+++ main.py
@@ -23,10 +23,350 @@ haplocheiruscmp = 0.0
 gualichocmp = 0.0
 
 def pawcmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
     herrerasauruscmp += abs(config.herrerasauruspaw - input)
-    
+    eoraptorcmp += abs(config.eoraptorpaw - input)
+    tawacmp += abs(config.tawapaw - input)
+    syntarsuscmp += abs(config.syntarsuspaw - input)
+    coelophysiscmp += abs(config.coelophysispaw - input)
+    dilophosauruscmp += abs(config.dilophosauruspaw - input)
+    eodromaeuscmp += abs(config.eodromaeuspaw - input)
+    carnotauruscmp += abs(config.carnotauruspaw - input)
+    africancmp += abs(config.africanpaw - input)
+    xuanhanosauruscmp += abs(config.xuanhanosauruspaw - input)
+    allosauruscmp += abs(config.allosauruspaw - input)
+    torvosauruscmp += abs(config.torvosauruspaw - input)
+    acrocanthosauruscmp += abs(config.acrocanthosauruspaw - input)
+    guanlongcmp += abs(config.guanlongpaw - input)
+    albertosauruscmp += abs(config.albertosauruspaw - input)
+    gorgosauruscmp += abs(config.gorgosauruspaw - input)
+    daspletosauruscmp += abs(config.daspletosauruspaw - input)
+    tarbosauruscmp += abs(config.tarbosauruspaw - input)
+    tyrannosauruscmp += abs(config.tyrannosauruspaw - input)
+    yutyrannuscmp += abs(config.yutyrannuspaw - input)
+    haplocheiruscmp += abs(config.haplocheiruspaw - input)
+    gualichocmp += abs(config.gualichopaw - input)
+
+def bicmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurusbi - input)
+    eoraptorcmp += abs(config.eoraptorbi - input)
+    tawacmp += abs(config.tawabi - input)
+    syntarsuscmp += abs(config.syntarsusbi - input)
+    coelophysiscmp += abs(config.coelophysisbi - input)
+    dilophosauruscmp += abs(config.dilophosaurusbi - input)
+    eodromaeuscmp += abs(config.eodromaeusbi - input)
+    carnotauruscmp += abs(config.carnotaurusbi - input)
+    africancmp += abs(config.africanbi - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurusbi - input)
+    allosauruscmp += abs(config.allosaurusbi - input)
+    torvosauruscmp += abs(config.torvosaurusbi - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurusbi - input)
+    guanlongcmp += abs(config.guanlongbi - input)
+    albertosauruscmp += abs(config.albertosaurusbi - input)
+    gorgosauruscmp += abs(config.gorgosaurusbi - input)
+    daspletosauruscmp += abs(config.daspletosaurusbi - input)
+    tarbosauruscmp += abs(config.tarbosaurusbi - input)
+    tyrannosauruscmp += abs(config.tyrannosaurusbi - input)
+    yutyrannuscmp += abs(config.yutyrannusbi - input)
+    haplocheiruscmp += abs(config.haplocheirusbi - input)
+    gualichocmp += abs(config.gualichobi - input)
+
+def heicmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurushei - input)
+    eoraptorcmp += abs(config.eoraptorhei - input)
+    tawacmp += abs(config.tawahei - input)
+    syntarsuscmp += abs(config.syntarsushei - input)
+    coelophysiscmp += abs(config.coelophysishei - input)
+    dilophosauruscmp += abs(config.dilophosaurushei - input)
+    eodromaeuscmp += abs(config.eodromaeushei - input)
+    carnotauruscmp += abs(config.carnotaurushei - input)
+    africancmp += abs(config.africanhei - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurushei - input)
+    allosauruscmp += abs(config.allosaurushei - input)
+    torvosauruscmp += abs(config.torvosaurushei - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurushei - input)
+    guanlongcmp += abs(config.guanlonghei - input)
+    albertosauruscmp += abs(config.albertosaurushei - input)
+    gorgosauruscmp += abs(config.gorgosaurushei - input)
+    daspletosauruscmp += abs(config.daspletosaurushei - input)
+    tarbosauruscmp += abs(config.tarbosaurushei - input)
+    tyrannosauruscmp += abs(config.tyrannosaurushei - input)
+    yutyrannuscmp += abs(config.yutyrannushei - input)
+    haplocheiruscmp += abs(config.haplocheirushei - input)
+    gualichocmp += abs(config.gualichohei - input)
+  
+def oicmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurusoi - input)
+    eoraptorcmp += abs(config.eoraptoroi - input)
+    tawacmp += abs(config.tawaoi - input)
+    syntarsuscmp += abs(config.syntarsusoi - input)
+    coelophysiscmp += abs(config.coelophysisoi - input)
+    dilophosauruscmp += abs(config.dilophosaurusoi - input)
+    eodromaeuscmp += abs(config.eodromaeusoi - input)
+    carnotauruscmp += abs(config.carnotaurusoi - input)
+    africancmp += abs(config.africanoi - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurusoi - input)
+    allosauruscmp += abs(config.allosaurusoi - input)
+    torvosauruscmp += abs(config.torvosaurusoi - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurusoi - input)
+    guanlongcmp += abs(config.guanlongoi - input)
+    albertosauruscmp += abs(config.albertosaurusoi - input)
+    gorgosauruscmp += abs(config.gorgosaurusoi - input)
+    daspletosauruscmp += abs(config.daspletosaurusoi - input)
+    tarbosauruscmp += abs(config.tarbosaurusoi - input)
+    tyrannosauruscmp += abs(config.tyrannosaurusoi - input)
+    yutyrannuscmp += abs(config.yutyrannusoi - input)
+    haplocheiruscmp += abs(config.haplocheirusoi - input)
+    gualichocmp += abs(config.gualichooi - input)
+
+def rricmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurusrri - input)
+    eoraptorcmp += abs(config.eoraptorrri - input)
+    tawacmp += abs(config.tawarri - input)
+    syntarsuscmp += abs(config.syntarsusrri - input)
+    coelophysiscmp += abs(config.coelophysisrri - input)
+    dilophosauruscmp += abs(config.dilophosaurusrri - input)
+    eodromaeuscmp += abs(config.eodromaeusrri - input)
+    carnotauruscmp += abs(config.carnotaurusrri - input)
+    africancmp += abs(config.africanrri - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurusrri - input)
+    allosauruscmp += abs(config.allosaurusrri - input)
+    torvosauruscmp += abs(config.torvosaurusrri - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurusrri - input)
+    guanlongcmp += abs(config.guanlongrri - input)
+    albertosauruscmp += abs(config.albertosaurusrri - input)
+    gorgosauruscmp += abs(config.gorgosaurusrri - input)
+    daspletosauruscmp += abs(config.daspletosaurusrri - input)
+    tarbosauruscmp += abs(config.tarbosaurusrri - input)
+    tyrannosauruscmp += abs(config.tyrannosaurusrri - input)
+    yutyrannuscmp += abs(config.yutyrannusrri - input)
+    haplocheiruscmp += abs(config.haplocheirusrri - input)
+    gualichocmp += abs(config.gualichorri - input)
+
+def mc3ricmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurusmc3ri - input)
+    eoraptorcmp += abs(config.eoraptormc3ri - input)
+    tawacmp += abs(config.tawamc3ri - input)
+    syntarsuscmp += abs(config.syntarsusmc3ri - input)
+    coelophysiscmp += abs(config.coelophysismc3ri - input)
+    dilophosauruscmp += abs(config.dilophosaurusmc3ri - input)
+    eodromaeuscmp += abs(config.eodromaeusmc3ri - input)
+    carnotauruscmp += abs(config.carnotaurusmc3ri - input)
+    africancmp += abs(config.africanmc3ri - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurusmc3ri - input)
+    allosauruscmp += abs(config.allosaurusmc3ri - input)
+    torvosauruscmp += abs(config.torvosaurusmc3ri - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurusmc3ri - input)
+    guanlongcmp += abs(config.guanlongmc3ri - input)
+    albertosauruscmp += abs(config.albertosaurusmc3ri - input)
+    gorgosauruscmp += abs(config.gorgosaurusmc3ri - input)
+    daspletosauruscmp += abs(config.daspletosaurusmc3ri - input)
+    tarbosauruscmp += abs(config.tarbosaurusmc3ri - input)
+    tyrannosauruscmp += abs(config.tyrannosaurusmc3ri - input)
+    yutyrannuscmp += abs(config.yutyrannusmc3ri - input)
+    haplocheiruscmp += abs(config.haplocheirusmc3ri - input)
+    gualichocmp += abs(config.gualichomc3ri - input)
+
+def haacmp(input):
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
+    herrerasauruscmp += abs(config.herrerasaurushaa - input)
+    eoraptorcmp += abs(config.eoraptorhaa - input)
+    tawacmp += abs(config.tawahaa - input)
+    syntarsuscmp += abs(config.syntarsushaa - input)
+    coelophysiscmp += abs(config.coelophysishaa - input)
+    dilophosauruscmp += abs(config.dilophosaurushaa - input)
+    eodromaeuscmp += abs(config.eodromaeushaa - input)
+    carnotauruscmp += abs(config.carnotaurushaa - input)
+    africancmp += abs(config.africanhaa - input)
+    xuanhanosauruscmp += abs(config.xuanhanosaurushaa - input)
+    allosauruscmp += abs(config.allosaurushaa - input)
+    torvosauruscmp += abs(config.torvosaurushaa - input)
+    acrocanthosauruscmp += abs(config.acrocanthosaurushaa - input)
+    guanlongcmp += abs(config.guanlonghaa - input)
+    albertosauruscmp += abs(config.albertosaurushaa - input)
+    gorgosauruscmp += abs(config.gorgosaurushaa - input)
+    daspletosauruscmp += abs(config.daspletosaurushaa - input)
+    tarbosauruscmp += abs(config.tarbosaurushaa - input)
+    tyrannosauruscmp += abs(config.tyrannosaurushaa - input)
+    yutyrannuscmp += abs(config.yutyrannushaa - input)
+    haplocheiruscmp += abs(config.haplocheirushaa - input)
+    gualichocmp += abs(config.gualichohaa - input)
 
 def main():
+    global herrerasauruscmp
+    global eoraptorcmp
+    global tawacmp
+    global syntarsuscmp
+    global coelophysiscmp
+    global dilophosauruscmp
+    global eodromaeuscmp
+    global carnotauruscmp
+    global africancmp
+    global xuanhanosauruscmp
+    global allosauruscmp
+    global torvosauruscmp
+    global acrocanthosauruscmp
+    global guanlongcmp
+    global albertosauruscmp
+    global gorgosauruscmp
+    global daspletosauruscmp
+    global tarbosauruscmp
+    global tyrannosauruscmp
+    global yutyrannuscmp
+    global haplocheiruscmp
+    global gualichocmp
     print("Hi! Welcome to the Dinosorter. Enter the requested measurement data to compare your specimen to our list of Theropods.")
     PAW = float(input("Enter PAW: "))
     BI = float(input("Enter BI: "))
@@ -37,6 +377,19 @@ def main():
     HAA = float(input("Enter HAA: "))
     print("calculating paw...")
     pawcmp(PAW)
+    print("calculating bi...")
+    bicmp(BI)
+    print("calculating hei...")
+    heicmp(HEI)
+    print("calculating oi...")
+    oicmp(OI)
+    print("calculating rri...")
+    rricmp(RRI)
+    print("calculating mc3ri...")
+    mc3ricmp(MC3RI)
+    print("calculating haa...")
+    haacmp(HAA)
+    print("determining closest measurements...")
 
 if __name__ == "__main__":
     main()
